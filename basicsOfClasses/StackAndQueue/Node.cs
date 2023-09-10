17007b62c639eb3c863d29f82dfc3881bbfec1ef
eyad <eyad.mansour.1997@gmail.com>
2023-09-10 11:27:37 +0300

    implemented the stack

---
+++ basicsOfClasses/StackAndQueue/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace StackAndQueue
+{
+    public class Node
+    {
+        public int Data { set; get; }
+        public Node Next {set; get;}
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
+
---
+++ basicsOfClasses/StackAndQueue/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace StackAndQueue
+{
+    public class Stack
+    {
+        private Node top;
+
+        public Stack()
+        {
+            top = null;
+        }
+
+        public bool isEmpty()
+        {
+            return top == null;
+        }
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.Next = top;
+            top = newNode;
+        }
+        public int Pop()
+        {
+            if (isEmpty())
+            {
+                throw new InvalidOperationException("stack is empty");
+            }
+            int data = top.Data;
+            top = top.Next;
+            return data;
+        }
+        public int Peek()
+        {
+            if (isEmpty())
+            {
+                throw new InvalidOperationException("stack is empty");
+            }
+            return top.Data;
+        }
+    }
+}
+
